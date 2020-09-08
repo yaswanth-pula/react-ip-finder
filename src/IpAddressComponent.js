de989d93e5c9e175c9de4f2bc8d057b949c89c66
YASWANTH SAI <35864576+Yaswanth899@users.noreply.github.com>
2020-09-08 17:36:26 +0530

    Created Header Commponent

---
+++ src/IpAddressComponent.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+function IpAddressComponent() {
+  const [ipAddress, setIpAddress] = useState({
+    isFetching: true,
+    ip: "0.0.0.0",
+  });
+  useEffect(() => {
+    const API_URL = "https://api64.ipify.org/?format=json";
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((json_data) => {
+        setIpAddress({ isFetching: false, ip: json_data.ip });
+      })
+      .catch(console.error);
+  }, [setIpAddress]);
+  return (
+    <div>
+      <h2>{ipAddress.ip}</h2>
+    </div>
+  );
+}
+export default IpAddressComponent;
