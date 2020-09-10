222ebe3f66b7c7b7c3794a921d4592cab9491db6
YASWANTH SAI <35864576+Yaswanth899@users.noreply.github.com>
2020-09-10 17:27:35 +0530

    Added material-ui container to App root

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import "./App.css";
 import NavHeader from "./NavHeader";
 import IpAddressComponent from "./IpAddressComponent";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
 
 function App() {
   return (
-    <div>
-      <NavHeader />
-      <IpAddressComponent />
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm" id="my-container">
+        <div>
+          <NavHeader />
+          <IpAddressComponent />
+        </div>
+      </Container>
+    </React.Fragment>
   );
 }
 
