767b9c031c0813bdf8e9486e053eeb7cd5c0e7b7
YASWANTH SAI <35864576+Yaswanth899@users.noreply.github.com>
2020-09-10 17:23:07 +0530

    Added Copy Button with Functionality

--- src/IpAddressComponent.js
+++ src/IpAddressComponent.js
@@ -1,27 +1,54 @@
 import React, { useState, useEffect } from "react";
+import Chip from "@material-ui/core/Chip";
+import { API_Config } from "./config.json";
+import { Button } from "@material-ui/core";
+
 function IpAddressComponent() {
-  const [ipAddress, setIpAddress] = useState({
+  // Hooks
+  const [ipAddressHook, setIpAddress] = useState({
     isFetching: true,
     ip: "0.0.0.0",
   });
   useEffect(() => {
-    const API_URL = "https://api64.ipify.org/?format=json";
-    fetch(API_URL)
+    fetch(API_Config.API_URL)
       .then((response) => response.json())
       .then((json_data) => {
         setIpAddress({ isFetching: false, ip: json_data.ip });
       })
       .catch(console.error);
   }, [setIpAddress]);
+
+  function loadIpAddress() {
+    if (ipAddressHook.isFetching) {
+      return <h2>Obtaining Ip Address...</h2>;
+    } else {
+      return <h2>{ipAddressHook.ip}</h2>;
+    }
+  }
+
+  function copyIpButtonClick() {
+    navigator.clipboard.writeText(ipAddressHook.ip);
+    //document.execCommand("copy");
+  }
+
   // return (
   //   <div>
   //     <h2>{ipAddress.ip}</h2>
   //   </div>
   // );
-  if (ipAddress.isFetching) {
-    return <h2>Obtaining Ip Address...</h2>;
-  } else {
-    return <h2>{ipAddress.ip}</h2>;
-  }
+  return (
+    <div>
+      <h2>Your Ip Address</h2>
+      <Chip id="my-chip" label={loadIpAddress()} />
+      <Button
+        variant="outlined"
+        size="small"
+        onClick={() => copyIpButtonClick()}
+        disabled={ipAddressHook.isFetching}
+      >
+        COPY
+      </Button>
+    </div>
+  );
 }
 export default IpAddressComponent;
