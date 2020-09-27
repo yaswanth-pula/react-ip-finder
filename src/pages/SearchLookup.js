cdefe91f469b93ad73ce1f3186392570511294c9
yaswanth-pula <35864576+Yaswanth899@users.noreply.github.com>
2020-09-27 16:43:35 +0530

    Added Lookup IP validation code
    -The validation of user search LookupIP has been taken care so that only
    Valid IP is taken further for API Call.

--- src/pages/SearchLookup.js
+++ src/pages/SearchLookup.js
@@ -5,35 +5,51 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import SearchIcon from "@material-ui/icons/Search";
 import ClientLookup from "./ClientLookup";
+import validateIP from "../services/ValidateIp";
+
 const SearchLookup = () => {
   ///Hooks
   const [lookUpIp, setLookIp] = useState("");
   const [canLoad, setCanLoad] = useState(false);
+  const [isValidInput, setIsValidInput] = useState(true);
   // utility Function
+  function handleSearchBtnClick(event) {
+    if (validateIP(lookUpIp)) {
+      setCanLoad(true);
+      setIsValidInput(true);
+    } else setIsValidInput(false);
+    event.preventDefault();
+  }
+
   return (
     <div>
       <Paper id="search-lookup-paper" className="center-align-items">
-        <TextField
-          className="search-input"
-          autoComplete="false"
-          id="outlined-basic"
-          label="Search Ip "
-          variant="outlined"
-          size="small"
-          onChange={(event) => {
-            setLookIp(event.target.value);
-            setCanLoad(false);
-          }}
-        />
-        <Button
-          id="button-style"
-          size="medium"
-          variant="outlined"
-          startIcon={<SearchIcon />}
-          onClick={() => setCanLoad(true)}
-        >
-          Search
-        </Button>
+        <form onSubmit={handleSearchBtnClick}>
+          <TextField
+            error={!isValidInput}
+            className="search-input"
+            autoComplete="false"
+            id="outlined-basic"
+            label="Search Ip "
+            variant="outlined"
+            size="small"
+            helperText={!isValidInput && "Invalid Ip"}
+            onChange={(event) => {
+              setLookIp(event.target.value);
+              setCanLoad(false);
+            }}
+          />
+          <Button
+            disabled={!lookUpIp}
+            id="button-style"
+            size="medium"
+            variant="outlined"
+            startIcon={<SearchIcon />}
+            onClick={handleSearchBtnClick}
+          >
+            Search
+          </Button>
+        </form>
       </Paper>
       <div>{canLoad && <ClientLookup ip={lookUpIp} />}</div>
     </div>
